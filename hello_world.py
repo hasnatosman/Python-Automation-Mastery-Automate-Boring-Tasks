9498516aa4478e2aa80363b58600d5b31ff79cfc
HASNAT <61647150+hasnatosman@users.noreply.github.com>
2026-04-23 23:26:39 +0600

    Hello World

---
+++ hello_world.py
@@ -0,0 +1,14 @@
+# def greet(name):
+#     return f'Hello, {name}! Welcome to Python Course.'
+#
+# if __name__ == '__main__':
+#     name = input('Enter the name: ')
+#     print(greet(name))
+
+import requests
+import pandas as pd
+import numpy as np
+
+print("Request version:", requests.__version__)
+print("Numpy version", np.__version__)
+print("Pandas version", pd.__version__)
